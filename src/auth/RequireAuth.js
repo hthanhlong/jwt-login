cec71a3cf3f643cd256272b35260599b657bf4c9
long.hoangs3corp <long.hoang@s3corp.com.vn>
2022-10-03 20:32:10 +0700

    update requireAuth

--- src/auth/RequireAuth.js
+++ src/auth/RequireAuth.js
@@ -4,8 +4,7 @@ import useAuth from "../hooks/useAuth";
 const RequireAuth = ({ allowedRoles }) => {
   const { auth } = useAuth();
   const location = useLocation();
-
-  if (!auth) return <Navigate to="/login" state={{ from: location }} replace />;
+  if (!auth || !Object.keys(auth).length) return <Navigate to="/login" state={{ from: location }} replace />;
   const isRole = auth?.roles?.find((role) => allowedRoles?.includes(role));
   if (isRole) return <Outlet />
   return <Navigate to="/unauthorized" state={{ from: location }} replace />;
